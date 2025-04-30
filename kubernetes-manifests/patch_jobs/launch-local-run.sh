d384bbf122b59f9934540a2901ed8d95ba7ac902
ladejib <ladejibiobaku@gmail.com>
2025-04-30 17:38:53 -0400

    incuded the yaml files

---
+++ kubernetes-manifests/patch_jobs/launch-local-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+RUN_ID="run-$(date +%s)"
+echo "Injecting RUN_ID: $RUN_ID"
+
+sed "s/__RUN_ID__/${RUN_ID}/g" job-merge-patch.yaml > ../manifests/job-merge.yaml
+sed "s/__RUN_ID__/${RUN_ID}/g" job-test-patch.yaml > ../manifests/job-test.yaml
