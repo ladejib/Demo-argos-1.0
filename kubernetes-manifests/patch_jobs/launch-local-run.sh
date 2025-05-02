fe1740f4dfd33dc16bf72b93303e23132a7bc3cb
ladejib <ladejibiobaku@gmail.com>
2025-05-02 13:27:39 -0400

    Created configMap for entrypoint

--- kubernetes-manifests/patch_jobs/launch-local-run.sh
+++ kubernetes-manifests/patch_jobs/launch-local-run.sh
@@ -6,4 +6,4 @@ echo "Injecting RUN_ID: $RUN_ID"
 
 sed "s/__RUN_ID__/${RUN_ID}/g" job-merge-patch.yaml > ../manifests/job-merge.yaml
 sed "s/__RUN_ID__/${RUN_ID}/g" job-test-patch.yaml > ../manifests/job-test.yaml
-sed "s/__RUN_ID__/${RUN_ID}/g" entrypoint-patch.sh > ../../../../playwright-tests/entrypoint.sh
+sed "s/__RUN_ID__/${RUN_ID}/g" entrypoint-configmap-patch.yaml > ../manifests/entrypoint-configmap.yaml
